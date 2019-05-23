2b432ff0fcf36ed4910b2564567855a133e01faf
Deepankar01 <deepankar01agrawal@gmail.com>
2019-05-23 16:31:57 +0530

    completed things to remember

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import './scss/tree.scss';
 import './scss/ourOffices.scss';
 import './scss/feelingBored.scss';
 import './scss/thankyou.scss';
+import './scss/thingsToRemember.scss';
 import { screens, DEFAULT_SCREEN_TO_SHOW } from './controlBox/constants';
 import { moveScreensOnKey } from './controlBox/navigation';
 import './controlBox/customAction';
