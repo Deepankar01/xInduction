c204b943c1ced1ef6722e7bd4db087e19027d982
Deepankar01 <deepankar01agrawal@gmail.com>
2019-05-23 15:08:57 +0530

    updated thankyou page

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import './scss/components.scss';
 import './scss/tree.scss';
 import './scss/ourOffices.scss';
 import './scss/feelingBored.scss';
+import './scss/thankyou.scss';
 import { screens, DEFAULT_SCREEN_TO_SHOW } from './controlBox/constants';
 import { moveScreensOnKey } from './controlBox/navigation';
 import './controlBox/customAction';
