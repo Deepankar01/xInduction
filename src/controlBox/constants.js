1a29c04edd1cbd76049f299674b53ad414feff04
Deepankar01 <deepankar01agrawal@gmail.com>
2019-05-23 13:16:52 +0530

    added feeling bored

--- src/controlBox/constants.js
+++ src/controlBox/constants.js
@@ -1,4 +1,4 @@
 export const screens = ['#splash', '#home', '#bornInNetherlands', "#organisationStructure", "#organizationMatrix", "#feelingBored", "#cultureOfInovation", "#toolsWeUse", "#thingsToRemember", "#thankyou"];
-export const DEFAULT_SCREEN_INDEX = 0;
+export const DEFAULT_SCREEN_INDEX = 5;
 export const DEFAULT_SCREEN_TO_SHOW = screens[DEFAULT_SCREEN_INDEX];
 export const SHOW_NAV_FROM_INDEX = 2;
--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import './scss/bornInNetherlands.scss';
 import './scss/components.scss';
 import './scss/tree.scss';
 import './scss/ourOffices.scss';
+import './scss/feelingBored.scss';
 import { screens, DEFAULT_SCREEN_TO_SHOW } from './controlBox/constants';
 import { moveScreensOnKey } from './controlBox/navigation';
 import './controlBox/customAction';
