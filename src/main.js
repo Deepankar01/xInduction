41f67f17e1879d021fbdac8f6339c64692da08cc
Deepankar01 <deepankar01agrawal@gmail.com>
2019-06-04 17:52:53 +0530

    added organization structure

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ import './scss/toolsBox.scss';
 import './scss/cultureOfInovation.scss';
 import './scss/culture.scss';
 import './scss/xebiaGroup.scss';
+import './scss/organizationStructure.scss';
 import { screens, DEFAULT_SCREEN_TO_SHOW } from './controlBox/constants';
 import { moveScreensOnKey } from './controlBox/navigation';
 import './controlBox/customAction';
