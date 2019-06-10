3ccd810c446ad9da195a1a9dd0b5373fc05ac682
Deepankar01 <deepankar01agrawal@gmail.com>
2019-06-10 19:23:36 +0530

    updated hover

--- src/controlBox/organizationMatrix.js
+++ src/controlBox/organizationMatrix.js
@@ -1,8 +1,6 @@
 
 $(".project-row").click((event) => {
     const xebian = $('.xebian');
-    $('.xebian').parent().removeClass('hover-xebian');
     const { target } = event;
-    $(target).addClass('hover-xebian');
     target.append(xebian[0]);
 });
